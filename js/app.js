c9b1c791c08dc93b5dad520d832172211fb0f2b5
laith <mr.layth_2011@hotmail.com>
2020-05-11 17:37:46 +0300

    creat a web

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+alert('Welcome To My Web Site ');
+
+var name =prompt('Enter Your Name');
+
+alert ('Welcome ' + name);
+
+alert('i would like to ask you a 5 questions please answer with y/n or yes/no');
+
+var hunt=prompt('Do i love hunting ? ');
+var hunt2=hunt.toLowerCase();
+console.log(hunt2);
+
+// var hunt=prompt('Do i love coffie ? ');
+// var hunt=prompt('Do i love mansaf ? ');
+// var hunt=prompt('Do i love playing game ? ');
+// var hunt=prompt('Do i love play football ? ');
+
+
+
+
+
+
+
+
+
+
+
+
+//alert ('Welcome ' + name);
